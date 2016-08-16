4b2a9b4435277770457ffa633e9805a749d1b0fe
Ģirts Ķesteris <irts esteris>
2016-08-16 18:09:37 +0300

    Initial Commit

---
+++ Assets/Src/Editor/NodeManagerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(NodeManager))]
+public class NodeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        NodeManager nodeManager = (NodeManager) target;
+        if (GUILayout.Button("Create Button"))
+        {
+            nodeManager.BuildObject();
+        }
+    }
+}
---
+++ Assets/Src/NodeManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NodeManager : MonoBehaviour
+{
+    private static NodeManager instance;
+
+    [SerializeField]
+    private Node[] nodes;
+
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this);
+    }
+
+    public void BuildObject()
+    {
+        List<Node> list = nodes.ToList();
+
+        Node node = CreateNode();
+        list.Add(node);
+
+        nodes = list.ToArray<Node>();
+    }
+
+    private Node CreateNode()
+    {
+        GameObject button = Instantiate(buttonTemplate, GetPositionForNode(), Quaternion.identity) as GameObject;
+        button.transform.SetParent(transform);
+        return button.GetComponent<Node>();
+    }
+
+    private Vector3 GetPositionForNode()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        return pos;
+    }
+
+}
