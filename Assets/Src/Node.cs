79d182ece21bc1263a402b77735f621cbc422607
Sacristan <girts.kesteris@gmail.com>
2016-08-17 20:27:34 +0300

    Refactored Node creation into Node.cs

--- Assets/Src/Node.cs
+++ Assets/Src/Node.cs
@@ -3,4 +3,33 @@ using System.Collections;
 
 public class Node : MonoBehaviour
 {
+    private NodeManager _nodeManager;
+    private int _index;
+
+    public static Node Create(GameObject nodeTemplate, NodeManager nodeManager)
+    {
+        GameObject nodeGO = Instantiate(nodeTemplate) as GameObject;
+
+        Node node = nodeGO.GetComponent<Node>();
+        node._nodeManager = nodeManager;
+        node._index = nodeManager.Nodes.Count;
+
+        node.Setup();
+
+        return nodeGO.GetComponent<Node>();
+    }
+
+    private void Setup()
+    {
+        transform.position = GetPosition();
+        gameObject.name = "Node" + _index;
+        transform.SetParent(_nodeManager.transform);
+    }
+
+    private Vector3 GetPosition()
+    {
+        Vector3 pos = _nodeManager.transform.position;
+        return pos;
+    }
+
 }
--- Assets/Src/NodeManager.cs
+++ Assets/Src/NodeManager.cs
@@ -13,7 +13,7 @@ public class NodeManager : MonoBehaviour
 
     public void AddNode()
     {
-        nodes.Add(CreateNode());
+        nodes.Add(Node.Create(buttonTemplate, this));
     }
 
     public void RemoveNodeAt(int index)
@@ -21,20 +21,6 @@ public class NodeManager : MonoBehaviour
         Node node = nodes[index];
         nodes.RemoveAt(index);
         DestroyImmediate(node.gameObject);
-    }
-
-    private Node CreateNode()
-    {
-        GameObject button = Instantiate(buttonTemplate, GetPositionForNode(), Quaternion.identity) as GameObject;
-        button.name = "Node" + (nodes.Count+1);
-        button.transform.SetParent(transform);
-        return button.GetComponent<Node>();
-    }
-
-    private Vector3 GetPositionForNode()
-    {
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        return pos;
     }
 
 }
